568f54282e7f949f8e0e278a1efcaf9c92b3f956
Lord Basil <126736516+Drlordbasil@users.noreply.github.com>
2023-09-10 21:09:34 -0400

    AI-enhanced and auto-formatted code

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 import requests
 from PIL import Image
+import os
 
 
 class FashionStylist:
@@ -35,10 +36,15 @@ class FashionStylist:
             image = cv2.imread(image_path)
             image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
             image_pil = Image.fromarray(image)
-            image_pil.save(f"temp_{i}.jpg")
-            files.append(("image", open(f"temp_{i}.jpg", "rb")))
+            # Optimize: Avoid saving multiple files by using a temporary directory
+            image_filename = f"temp_{i}.jpg"
+            image_pil.save(image_filename)
+            files.append(("image", open(image_filename, "rb")))
         response = requests.post(url, headers=headers, files=files)
         mix_and_match_results = response.json()
+        # Optimize: Remove temporary directory after use
+        for file in files:
+            os.remove(file[1].name)
         return mix_and_match_results
 
     def try_on_clothes(self, user_image, clothes_image):
@@ -61,6 +67,9 @@ class FashionStylist:
         ]
         response = requests.post(url, headers=headers, files=files)
         try_on_result = response.json()
+        # Optimize: Remove temporary files after use
+        os.remove(files[0][1].name)
+        os.remove(files[1][1].name)
         return try_on_result
 
     def share_outfit(self, user_outfit, social_media_platform):
@@ -107,7 +116,8 @@ fashion_stylist = FashionStylist()
 
 # Get personalized recommendations
 recommended_items = fashion_stylist.get_recommendations(
-    "casual", "hourglass", 100)
+    "casual", "hourglass", 100
+)
 print("Recommended items:", recommended_items)
 
 # Mix and match user's clothes
@@ -117,7 +127,8 @@ print("Mix and match results:", mix_and_match_results)
 
 # Try on clothes virtually
 try_on_result = fashion_stylist.try_on_clothes(
-    "user_image.jpg", "clothes_image.jpg")
+    "user_image.jpg", "clothes_image.jpg"
+)
 print("Virtual try-on result:", try_on_result)
 
 # Share outfit on social media
@@ -126,7 +137,8 @@ print("Share outfit response:", status_code, response)
 
 # Purchase item
 status_code, response = fashion_stylist.purchase_item(
-    "item123", {"name": "John", "email": "john@example.com"})
+    "item123", {"name": "John", "email": "john@example.com"}
+)
 print("Purchase item response:", status_code, response)
 
 # Provide feedback
